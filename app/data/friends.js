e82f00e876b3ca79d154851d958e9684c3a64bd1
deniskag <deniskagg@gmail.com>
2018-05-11 00:09:34 -0400

    changes

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+module.exports =  friends = [ {
+	"name" : "Benji",
+	"photo" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfAkK39UuNLAj_fYBOHf-HTtFUzHbUb6nubiWL9e5M4yqO0JZw5A",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Carl",
+	"photo" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSwn99YhTrMf9q-IGJffnxWG3tVc5-SiLrPlNFe5-7zKAjdhKi",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Lou",
+	"photo" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiQxDdQnSHnKjtPeuNuzRR3Bxvh6Ua_7mEHipWofdlt0RHS9d4",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
